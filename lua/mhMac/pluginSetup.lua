e9273cda139f209ed8d3d2156e26e423e687d14f
Michael Hegner <hegner123@gmail.com>
2026-01-06 09:29:27 -0500

    removed depricated require for pyright

--- lua/mhMac/pluginSetup.lua
+++ lua/mhMac/pluginSetup.lua
@@ -17,7 +17,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup(plugins)
 require 'nvim-web-devicons'.get_icons()
-require 'lspconfig'.pyright.setup {}
 autocmd('LspAttach', {
         group = mhNvim,
         callback = function(e)
